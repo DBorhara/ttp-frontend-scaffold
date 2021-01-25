5528f0687d9c42d59ccff4651bb0e4234d5a3767
Depak Borhara <depakborhara@gmail.com>
2021-01-24 21:55:26 -0500

    Cleaned up files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App/App';
+import './index.css';
 
 //Redux
 import { Provider } from 'react-redux';
